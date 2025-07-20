b59d3aafc867b5ef521d35594330b3dad0fb3055
wildfireOfMine <iamtheanguera@gmail.com>
2025-07-20 20:18:50 +0200

    Page 76 on the book. Sending emails

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 # Create your models here.
@@ -15,7 +16,10 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish'    
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -42,3 +46,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+                ]
+        )
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,37 @@
+from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 from django.shortcuts import get_object_or_404, render
 from .models import *
 from django.http import Http404 
+from django.views.generic import ListView
+from .forms import *
 
 # Create your views here.
 
-def post_list(request):
-    posts = Post.published.all()
+""" def post_list(request):
+    post_list = Post.published.all()
+
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     return render(
         request, 'blog/post/list.html', {'posts':posts}
-    )
+    ) """
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     try:
         post = get_object_or_404(
             Post,
-            id=id,
-            status=Post.Status.PUBLISHED
+            status=Post.Status.PUBLISHED,
+            slug=post,
+            publish__year=year,
+            publish__month=month,
+            publish__day=day
         )
     except Post.DoesNotExist:
         raise Http404("No Post found.")
@@ -25,3 +40,33 @@ def post_detail(request, id):
         'blog/post/detail.html',
         {'post':post}
     )
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        "blog/post/share.html",
+        {
+            "post":post,
+            "form":form,
+        }
+    )
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
