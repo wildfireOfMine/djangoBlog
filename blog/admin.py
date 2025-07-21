f7bb761f4b7dc55022ede20fa672e6fa54f7500c
wildfireOfMine <iamtheanguera@gmail.com>
2025-07-21 20:57:57 +0200

    Page 102, almost finished chapter 2

--- blog/admin.py
+++ blog/admin.py
@@ -13,3 +13,9 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
     show_facets = admin.ShowFacets.ALWAYS
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "post", "created", "active"]
+    list_filter = ["active", "created", "updated"]
+    search_fields = ["name", "email", "body"]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 from django.shortcuts import get_object_or_404, render
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 from .models import *
 from django.http import Http404 
 from django.views.generic import ListView
@@ -33,12 +35,20 @@ def post_detail(request, year, month, day, post):
             publish__month=month,
             publish__day=day
         )
+
+        comments = post.comments.filter(active=True)
+
+        form = CommentForm()
     except Post.DoesNotExist:
         raise Http404("No Post found.")
     return render(
         request,
         'blog/post/detail.html',
-        {'post':post}
+        {
+            'post':post,
+            "comments":comments,
+            "form":form,
+            }
     )
 
 def post_share(request, post_id):
@@ -47,11 +57,34 @@ def post_share(request, post_id):
         id=post_id,
         status=Post.Status.PUBLISHED
     )
+    sent = False
 
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+
+            subject = (
+                f"{cd['name']} ({cd['email']})"
+                f"recommends you read {post.title}"
+            )
+
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
     else:
         form = EmailPostForm()
 
@@ -61,6 +94,7 @@ def post_share(request, post_id):
         {
             "post":post,
             "form":form,
+            "sent":sent,
         }
     )
 
@@ -70,3 +104,30 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = "blog/post/list.html"
 
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+
+        comment.post = post
+        
+        comment.save()
+    
+    return render(
+        request,
+        "blog/post/comment.html",
+        {
+            "post":post,
+            "form":form,
+            "comment":comment,
+        }
+    )
